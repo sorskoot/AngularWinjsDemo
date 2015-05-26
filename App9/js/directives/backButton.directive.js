f9a3081660ee2c08431ee283d1230fd04ea64ead
Sorskoot <mental_c@hotmail.com>
2015-05-26 09:58:56 +0200

    css + backbutton

---
+++ App9/js/directives/backButton.directive.js
@@ -0,0 +1,16 @@
+﻿(function (angular) {
+    "use strict";
+    
+    angular.module(appConfig.appName).directive('ttBackButton', ttBackButton);
+
+    function ttBackButton() {
+        return {
+            restrict: "E",
+            replace: true,
+            template: '<button data-win-control="WinJS.UI.BackButton"></button>',
+            link: function (scope, element, attrs) {
+                WinJS.UI.process(element[0]);
+            }
+        }
+    }
+})(angular);
